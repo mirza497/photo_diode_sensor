241c0116907e394cae5b4b866f6faeee2cd1a2bb
Zeeshan <zbeigjmi@gmail.com>
2026-05-16 15:03:32 +0530

    added me.h file

---
+++ me.h
@@ -0,0 +1,26 @@
+/*
+ * me.h
+ *
+ *  Created on: 06-May-2026
+ *      Author: Zeshahan
+ */
+
+#ifndef ME_H_
+#define ME_H_
+
+/* ADC thresholds (12-bit, 3.3V) */
+#define ADC_100      41
+#define ADC_1K       410
+#define ADC_2K       820
+#define ADC_3K       1230
+#define ADC_4K       1640
+#define ADC_5K       2050
+#define ADC_6K       2460
+#define ADC_7K       2870
+#define ADC_8K       3280
+#define ADC_9K       3636
+#define ADC_10K      4095   // clamp
+
+
+
+#endif /* ME_H_ */
